5f2ab1f27c188f295de860f49ad6e21d7a5b5adc
tianyi <styzf@qq.com>
2019-12-25 15:06:50 +0800

    modified readme

--- app/src/main/java/com/sty/ne/aopdb/MainActivity.java
+++ app/src/main/java/com/sty/ne/aopdb/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity implements DBOperation{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //代理
         db = (DBOperation) Proxy.newProxyInstance(DBOperation.class.getClassLoader(),
                 new Class[]{DBOperation.class}, new DBHandler(this));
 
